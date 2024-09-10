51bf5570556fce61d388e5a64f491a77caf22f02
bill <yibie_jingnian@163.com>
2024-09-10 16:04:27 +0800

    feat: Fetch products by fetch function

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { ProductsTable } from './products-table';
-import { getProducts } from '@/lib/db';
+// import { getProducts } from '@/lib/db';
 
 export default async function ProductsPage({
   searchParams
@@ -11,10 +11,18 @@ export default async function ProductsPage({
 }) {
   const search = searchParams.q ?? '';
   const offset = searchParams.offset ?? 0;
-  const { products, newOffset, totalProducts } = await getProducts(
-    search,
-    Number(offset)
-  );
+  // const { products, newOffset, totalProducts } = await getProducts(
+  //   search,
+  //   Number(offset)
+  // );
+
+  const ret = await fetch('http://localhost:3000/products');
+  const products = await ret.json();
+
+  const { newOffset, totalProducts } = {
+    newOffset: 0,
+    totalProducts: 0
+  };
 
   return (
     <Tabs defaultValue="all">
